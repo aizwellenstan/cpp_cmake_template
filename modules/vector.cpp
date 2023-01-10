a35d1ba8a34b2055f550a610ceeb8c85800d4d4e
aizwellenstan <aizwellenstan@gmail.com>
2023-01-10 14:33:35 +0800

    refined

--- modules/vector.cpp
+++ modules/vector.cpp
@@ -1,8 +1,7 @@
 #include "vector.h"
 std::vector<std::string> AppendToVector(std::vector<std::string> v, std::string x) {
-    if(!(std::find(v.begin(), v.end(), x) != v.end())) {
+    if(!(std::find(v.begin(), v.end(), x) != v.end()))
         v.push_back(x);
-    }
     return v;
 }
 
@@ -10,9 +9,7 @@ std::vector<std::string> WordToVector(std::string s) {
     std::string tmp; 
     std::stringstream ss(s);
     std::vector<std::string> words;
-
-    while(getline(ss, tmp, ',')){
+    while(getline(ss, tmp, ','))
         words.push_back(tmp);
-    }
     return words;
 }
